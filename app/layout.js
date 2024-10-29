9302ecc573ed69e69312e7e6fcbb8927a8109def
sandy <sandeshjadhav2002@gmail.com>
2024-10-30 02:00:58 +0530

    footer done

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({subsets:["latin"]})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Zscrum",
@@ -14,7 +14,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
         <ThemeProvider attribute="class" defaultTheme="dark">
-          {children}
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-gray-900 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200 ">
+              <p>Made with 💗 by Sandesh Jadhav</p>
+            </div>
+          </footer>
         </ThemeProvider>
       </body>
     </html>
