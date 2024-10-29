efcae23e01412fcaf8d14a97788a1ea2fdfd9fd3
sandy <sandeshjadhav2002@gmail.com>
2024-10-30 02:55:56 +0530

    Header done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +14,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider 
+    appearance={{
+      baseTheme:shadesOfPurple
+    }}
+    >
     <html lang="en">
       <body className={`${inter.className}`}>
         <ThemeProvider attribute="class" defaultTheme="dark">
+          <Header />
           <main className="min-h-screen">{children}</main>
           <footer className="bg-gray-900 py-12">
             <div className="container mx-auto px-4 text-center text-gray-200 ">
@@ -23,5 +32,6 @@ export default function RootLayout({ children }) {
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
