eeed598c855ee150d4055a6a425695da99992381
sandy <sandeshjadhav2002@gmail.com>
2024-10-30 01:56:33 +0530

    font fix

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Inter } from 'next/font/google'
 
-const inter = Inter
+const inter = Inter({subsets:["latin"]})
 
 export const metadata = {
   title: "Zscrum",
