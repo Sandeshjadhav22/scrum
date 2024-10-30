924a016b609037b1cb23b8263ee7313c1292d611
sandy <sandeshjadhav2002@gmail.com>
2024-10-31 00:42:55 +0530

    User login button component

--- components/header.jsx
+++ components/header.jsx
@@ -1,44 +1,49 @@
 import React from "react";
-import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 // import { checkUser } from "@/lib/checkUser";
 // import UserMenu from "./user-menu";
 import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
 
 async function Header() {
-//   await checkUser();
+  //   await checkUser();
 
   return (
-    <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
-      <Link href="/" className="flex items-center">
-        <Image
-          src="/logo.png"
-          width="150"
-          height="60"
-          alt="Schedulrr Logo"
-          className="h-16 w-auto"
-        />
-      </Link>
-
-      <div className="flex items-center gap-4">
-        <Link href="/events?create=true">
-          <Button variant="default" className="flex items-center gap-2">
-            <PenBox size={18} />
-            <span className="hidden sm:inline">Create Event</span>
-          </Button>
+    <header className=" container mx-auto">
+      <nav className=" py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo2.png"}
+            width={200}
+            height={56}
+            className="h-10 w-auto object-contain"
+            alt="logo"
+          />
         </Link>
-        <SignedOut>
-          <SignInButton forceRedirectUrl="/dashboard">
+
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive" className="flex items-center gap-2 ">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
             <Button variant="outline">Login</Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          {/* <UserMenu /> */}
-        </SignedIn>
-      </div>
-    </nav>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            {/* <UserButton /> */}
+            <UserMenu/>
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 }
 
