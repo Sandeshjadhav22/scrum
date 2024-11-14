1af8754031215f9d2b4533bb0a0e329b3f19f659
sandy <sandeshjadhav2002@gmail.com>
2024-11-15 00:02:45 +0530

    react hook form

---
+++ app/lib/validators.js
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const projectSchema = z.object({
+  name: z
+    .string()
+    .min(1, "Project name is required")
+    .max(100, "project name must be 100 characters or less"),
+  key: z
+    .string()
+    .min(2, "Project key must be atleast 2 characters")
+    .max(10, "Project key must be 10 character or less"),
+  description: z
+    .string()
+    .max(500, "Description must be 500 character or less")
+    .optional(),
+});
